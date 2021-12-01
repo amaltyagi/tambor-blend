841ae2955ebeb667528fdf463072bbfdd185288e
amaltyagi <amalktyagi@gmail.com>
2021-11-30 21:17:05 -0500

    redirect

--- app.js
+++ app.js
@@ -83,8 +83,6 @@ app.get('/callback', function(req, res) {
 
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        console.log('redirect');
-        res.redirect('https://tambor-party.herokuapp.com/playlists.html/');
 
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
@@ -113,7 +111,8 @@ app.get('/callback', function(req, res) {
             refresh_token: refresh_token
           }));
 
-          
+        console.log('redirect');
+        res.redirect('https://tambor-party.herokuapp.com/playlists.html/');
       } else {
         res.redirect('/#' +
           querystring.stringify({
